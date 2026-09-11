d61da049a6178f4e36315d8182f9959bce1035b7
Afnan K. <inbox.afnankhan@gmail.com>
2026-09-11 16:25:29 +0500

    fix: Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,12 +35,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.svg", type: "image/svg+xml" },
-      { url: "/favicon.ico", sizes: "any" },
       { url: "/icons/icon-48.png", sizes: "48x48", type: "image/png" },
       { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
     ],
     apple: [{ url: "/apple-icon", sizes: "180x180", type: "image/png" }],
-    shortcut: "/favicon.svg",
   },
   manifest: "/site.webmanifest",
   other: {
